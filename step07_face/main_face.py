8759570c9428eef120fdc4141f9878b6e816f436
Hiroe HASHIGUCHI <55542434+hsgucci404@users.noreply.github.com>
2022-02-25 21:23:14 +0900

    Create main_face.py

--- step07_face/main_face.py
+++ step07_face/main_face.py
@@ -30,9 +30,16 @@ def main():
     current_time = time.time()  # 現在時刻の保存変数
     pre_time = current_time     # 5秒ごとの'command'送信のための時刻変数
 
+    # SDKバージョンを問い合わせ
+    sdk_ver = tello.query_sdk_version()
+
+    # モータとカメラの切替フラグ
     motor_on = False                    # モータON/OFFのフラグ
     camera_dir = Tello.CAMERA_FORWARD   # 前方/下方カメラの方向のフラグ
 
+    # 前回強制終了して下方カメラかもしれないので
+    if sdk_ver == '30':                                     # SDK 3.0に対応しているか？ 
+        tello.set_video_direction(Tello.CAMERA_FORWARD)     # カメラは前方に
     # 自動モードフラグ
     auto_mode = 0
 
@@ -44,16 +51,16 @@ def main():
         # 永久ループで繰り返す
         while True:
 
-            # (A) 画像取得
+            # (1) 画像取得
             image = frame_read.frame    # 映像を1フレーム取得しimage変数に格納
 
-            # (B) 画像サイズ変更と、カメラ方向による回転
+            # (2) 画像サイズ変更と、カメラ方向による回転
             small_image = cv2.resize(image, dsize=(480,360) )   # 画像サイズを半分に変更
 
             if camera_dir == Tello.CAMERA_DOWNWARD:     # 下向きカメラは画像の向きが90度ずれている
                 small_image = cv2.rotate(small_image, cv2.ROTATE_90_CLOCKWISE)      # 90度回転して、画像の上を前方にする
 
-            # (C) ここから画像処理
+            # (3) ここから画像処理
             # 5フレームに１回顔認識処理をする
             if cnt_frame >= 5:
                 # 顔検出のためにグレイスケール画像に変換，ヒストグラムの平坦化もかける
@@ -122,10 +129,10 @@ def main():
 
             cnt_frame += 1  # フレームを+1枚
 
-            # (X) ウィンドウに表示
+            # (4) ウィンドウに表示
             cv2.imshow('OpenCV Window', small_image)    # ウィンドウに表示するイメージを変えれば色々表示できる
 
-            # (Y) OpenCVウィンドウでキー入力を1ms待つ
+            # (5) OpenCVウィンドウでキー入力を1ms待つ
             key = cv2.waitKey(1) & 0xFF
             if key == 27:                   # key が27(ESC)だったらwhileループを脱出，プログラム終了
                 break
@@ -153,27 +160,29 @@ def main():
             elif key == ord('p'):           # ステータスをprintする
                 print(tello.get_current_state())
             elif key == ord('m'):           # モータ始動/停止を切り替え
-                if motor_on == False:       # 停止中なら始動 
-                    tello.turn_motor_on()
-                    motor_on = True
-                else:                       # 回転中なら停止
-                    tello.turn_motor_off()
-                    motor_on = False
+                if sdk_ver == '30':         # SDK 3.0に対応しているか？
+                    if motor_on == False:       # 停止中なら始動 
+                        tello.turn_motor_on()
+                        motor_on = True
+                    else:                       # 回転中なら停止
+                        tello.turn_motor_off()
+                        motor_on = False
             elif key == ord('c'):           # カメラの前方/下方の切り替え
-                if camera_dir == Tello.CAMERA_FORWARD:     # 前方なら下方へ変更
-                    tello.set_video_direction(Tello.CAMERA_DOWNWARD)
-                    camera_dir = Tello.CAMERA_DOWNWARD     # フラグ変更
-                else:                                      # 下方なら前方へ変更
-                    tello.set_video_direction(Tello.CAMERA_FORWARD)
-                    camera_dir = Tello.CAMERA_FORWARD      # フラグ変更
-                time.sleep(0.5)     # 映像が切り替わるまで少し待つ
+                if sdk_ver == '30':         # SDK 3.0に対応しているか？
+                    if camera_dir == Tello.CAMERA_FORWARD:     # 前方なら下方へ変更
+                        tello.set_video_direction(Tello.CAMERA_DOWNWARD)
+                        camera_dir = Tello.CAMERA_DOWNWARD     # フラグ変更
+                    else:                                      # 下方なら前方へ変更
+                        tello.set_video_direction(Tello.CAMERA_FORWARD)
+                        camera_dir = Tello.CAMERA_FORWARD      # フラグ変更
+                    time.sleep(0.5)     # 映像が切り替わるまで少し待つ
             elif key == ord('1'):
                 auto_mode = 1                    # 追跡モードON
             elif key == ord('0'):
                 tello.send_rc_control( 0, 0, 0, 0 )
                 auto_mode = 0                    # 追跡モードOFF
 
-            # (Z) 10秒おきに'command'を送って、死活チェックを通す
+            # (6) 10秒おきに'command'を送って、死活チェックを通す
             current_time = time.time()                          # 現在時刻を取得
             if current_time - pre_time > 10.0 :                 # 前回時刻から10秒以上経過しているか？
                 tello.send_command_without_return('command')    # 'command'送信
@@ -184,13 +193,15 @@ def main():
 
     # 終了処理部
     cv2.destroyAllWindows()                             # すべてのOpenCVウィンドウを消去
-    tello.set_video_direction(Tello.CAMERA_FORWARD)     # カメラは前方に戻しておく
+    
+    if sdk_ver == '30':                                 # SDK 3.0に対応しているか？
+        tello.set_video_direction(Tello.CAMERA_FORWARD) # カメラは前方に戻しておく
+
     tello.streamoff()                                   # 画像転送を終了(熱暴走防止)
     frame_read.stop()                                   # 画像受信スレッドを止める
 
-    del tello.background_frame_read                    # フレーム受信のインスタンスを削除    
+    del tello.background_frame_read                     # フレーム受信のインスタンスを削除    
     del tello                                           # telloインスタンスを削除
-
 
 # "python3 main_core.py"として実行された時だけ動く様にするおまじない処理
 if __name__ == "__main__":      # importされると__name_に"__main__"は入らないので，pyファイルが実行されたのかimportされたのかを判断できる．
