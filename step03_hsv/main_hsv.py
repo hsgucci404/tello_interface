197fffd7b758789275b51b37a9dd01f04bffd52f
Hiroe HASHIGUCHI <55542434+hsgucci404@users.noreply.github.com>
2022-02-25 18:07:33 +0900

    Update main_hsv.py

--- step03_hsv/main_hsv.py
+++ step03_hsv/main_hsv.py
@@ -23,8 +23,16 @@ def main():
     current_time = time.time()  # 現在時刻の保存変数
     pre_time = current_time     # 5秒ごとの'command'送信のための時刻変数
 
+    # SDKバージョンを問い合わせ
+    sdk_ver = tello.query_sdk_version()
+
+    # モータとカメラの切替フラグ
     motor_on = False                    # モータON/OFFのフラグ
     camera_dir = Tello.CAMERA_FORWARD   # 前方/下方カメラの方向のフラグ
+
+    # 前回強制終了して下方カメラかもしれないので
+    if sdk_ver == '30':                                     # SDK 3.0に対応しているか？ 
+        tello.set_video_direction(Tello.CAMERA_FORWARD)     # カメラは前方に
 
     # トラックバーを作るため，まず最初にウィンドウを生成
     cv2.namedWindow("OpenCV Window")
@@ -107,20 +115,22 @@ def main():
             elif key == ord('p'):           # ステータスをprintする
                 print(tello.get_current_state())
             elif key == ord('m'):           # モータ始動/停止を切り替え
-                if motor_on == False:       # 停止中なら始動 
-                    tello.turn_motor_on()
-                    motor_on = True
-                else:                       # 回転中なら停止
-                    tello.turn_motor_off()
-                    motor_on = False
+                if sdk_ver == '30':         # SDK 3.0に対応しているか？
+                    if motor_on == False:       # 停止中なら始動 
+                        tello.turn_motor_on()
+                        motor_on = True
+                    else:                       # 回転中なら停止
+                        tello.turn_motor_off()
+                        motor_on = False
             elif key == ord('c'):           # カメラの前方/下方の切り替え
-                if camera_dir == Tello.CAMERA_FORWARD:     # 前方なら下方へ変更
-                    tello.set_video_direction(Tello.CAMERA_DOWNWARD)
-                    camera_dir = Tello.CAMERA_DOWNWARD     # フラグ変更
-                else:                                      # 下方なら前方へ変更
-                    tello.set_video_direction(Tello.CAMERA_FORWARD)
-                    camera_dir = Tello.CAMERA_FORWARD      # フラグ変更
-                time.sleep(0.5)     # 映像が切り替わるまで少し待つ
+                if sdk_ver == '30':         # SDK 3.0に対応しているか？
+                    if camera_dir == Tello.CAMERA_FORWARD:     # 前方なら下方へ変更
+                        tello.set_video_direction(Tello.CAMERA_DOWNWARD)
+                        camera_dir = Tello.CAMERA_DOWNWARD     # フラグ変更
+                    else:                                      # 下方なら前方へ変更
+                        tello.set_video_direction(Tello.CAMERA_FORWARD)
+                        camera_dir = Tello.CAMERA_FORWARD      # フラグ変更
+                    time.sleep(0.5)     # 映像が切り替わるまで少し待つ
 
             # (Z) 10秒おきに'command'を送って、死活チェックを通す
             current_time = time.time()                          # 現在時刻を取得
@@ -133,14 +143,17 @@ def main():
 
     # 終了処理部
     cv2.destroyAllWindows()                             # すべてのOpenCVウィンドウを消去
-    tello.set_video_direction(Tello.CAMERA_FORWARD)     # カメラは前方に戻しておく
+    
+    if sdk_ver == '30':                                 # SDK 3.0に対応しているか？
+        tello.set_video_direction(Tello.CAMERA_FORWARD) # カメラは前方に戻しておく
+
     tello.streamoff()                                   # 画像転送を終了(熱暴走防止)
     frame_read.stop()                                   # 画像受信スレッドを止める
 
-    del tello.background_frame_read                    # フレーム受信のインスタンスを削除    
+    del tello.background_frame_read                     # フレーム受信のインスタンスを削除    
     del tello                                           # telloインスタンスを削除
 
 
-# "python3 main_core.py"として実行された時だけ動く様にするおまじない処理
+# "python3 main_hsv.py"として実行された時だけ動く様にするおまじない処理
 if __name__ == "__main__":      # importされると__name_に"__main__"は入らないので，pyファイルが実行されたのかimportされたのかを判断できる．
     main()    # メイン関数を実行
